2eeaae2795483731f2747dbc2fc408183ae26604
Ilya Tate <ilyatate@gmail.com>
2021-09-20 14:06:37 -0700

    Added some base stuff

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './Pages/index';
+import './Components/index';
+
+const App = () => {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Pokemon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pokemon = () => {
+
+  return (
+    <div className="Pokemon">
+
+    </div>
+  );
+}
+
+export default Pokemon;
